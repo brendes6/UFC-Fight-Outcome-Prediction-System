3a2e89c347b94040d445feeebdb93f8192bb588f
brendes6 <brendes6@gmail.com>
2025-05-29 18:09:04 -0400

    Update requirements

--- src/scrape_odds.py
+++ src/scrape_odds.py
@@ -1,9 +1,6 @@
-import pandas as pd
 from bs4 import BeautifulSoup
 import time
-import random
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
